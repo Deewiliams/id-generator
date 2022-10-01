c6f1745d603fb85ba818d9bc606fbcd1e64582f4
Desire <desireirankundawilliams@gmail.com>
2022-10-01 21:05:51 +0200

    able to get multiple input values

--- idgenerate.js
+++ idgenerate.js
@@ -1,12 +1,42 @@
+// function getUser(){
+//     const inputValues = document.querySelector('#myForm').value;
+//     console.log('information',inputValues);
+//     let today = new Date();
+//     let year = today.getFullYear()
+//     let uniqueId = Date.now()
 
-function getUser(){
-    const inputValues = document.querySelector("input").value;
-    let today = new Date();
-    let year = today.getFullYear()
-    let uniqueId = Date.now()
+//     const displayInfo = document.createElement("li");
+//     const textNode = document.createTextNode(inputValues + year + uniqueId);
+//     displayInfo.appendChild(textNode);
+//     document.getElementById('generatedId').appendChild(displayInfo);
+// }
+let today = new Date();
+let year = today.getFullYear();
+let dateNow = Date.now();
+let uniqueId = year + dateNow
 
-    const displayInfo = document.createElement("li");
-    const textNode = document.createTextNode(inputValues + year + uniqueId);
-    displayInfo.appendChild(textNode);
-    document.getElementById('generatedId').appendChild(displayInfo);
+function getUser() {
+    console.log('id',uniqueId);
+  let fullName = document.getElementById("fullName").value;
+  let age = document.getElementById("age").value;
+  let gender = document.getElementById("age").value;
+  let address = document.getElementById("age").value;
+  let state = document.getElementById("age").value;
+  let mobile = document.getElementById("age").value;
+  alert(
+    "fullName " +
+      fullName +
+      " age " +
+      age +
+      "gender" +
+      gender +
+      "address" +
+      address +
+      " state" +
+      state + uniqueId
+    //   +
+    //   "mobile" +
+    //   mobile
+    //   + 'id number' + uniqueId
+  );
 }
