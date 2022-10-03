05535b39af7e2fb375360cb159646709b8781d08
Desire <desireirankundawilliams@gmail.com>
2022-10-03 21:01:49 +0200

    added validation to the input fields

--- idgenerate.js
+++ idgenerate.js
@@ -13,35 +13,39 @@ function getUser() {
   let state = document.getElementById("state").value;
   let mobile = document.getElementById("mobile").value;
 
-  inputStorage.push({
-    fullname: fullName,
-    age: age,
-    gender: gender,
-    address: address,
-    state: state,
-    mobile: mobile,
-    uniqueId: uniqueId,
-  });
+  if (fullName == "" || age == "" || gender == "" || address == "" || state == "" || mobile == "" ) {
+    alert("Please make sure you fill all inputs");
+  } else {
+    inputStorage.push({
+      fullname: fullName,
+      age: age,
+      gender: gender,
+      address: address,
+      state: state,
+      mobile: mobile,
+      uniqueId: uniqueId,
+    });
+    
+    for (let i = 0; i < inputStorage.length; i++) {
+      const template = document.querySelector("#template");
+      const lists = document.querySelector(".lists");
   
-  for (let i = 0; i < inputStorage.length; i++) {
-    const template = document.querySelector("#template");
-    const lists = document.querySelector(".lists");
-
-    const clone = template.content.cloneNode(true);
-
-    clone.querySelector(".template-name").textContent =
-      inputStorage[i].fullname;
-    clone.querySelector(".template-age").textContent = inputStorage[i].age;
-    clone.querySelector(".template-gender").textContent =
-      inputStorage[i].gender;
-    clone.querySelector(".template-address").textContent =
-      inputStorage[i].address;
-    clone.querySelector(".template-state").textContent = inputStorage[i].state;
-    clone.querySelector(".template-mobile").textContent =
-      inputStorage[i].mobile;
-    clone.querySelector(".template-uniqueId").textContent =
-      inputStorage[i].uniqueId;
-    lists.append(clone);
+      const clone = template.content.cloneNode(true);
+  
+      clone.querySelector(".template-name").textContent =
+        inputStorage[i].fullname;
+      clone.querySelector(".template-age").textContent = inputStorage[i].age;
+      clone.querySelector(".template-gender").textContent =
+        inputStorage[i].gender;
+      clone.querySelector(".template-address").textContent =
+        inputStorage[i].address;
+      clone.querySelector(".template-state").textContent = inputStorage[i].state;
+      clone.querySelector(".template-mobile").textContent =
+        inputStorage[i].mobile;
+      clone.querySelector(".template-uniqueId").textContent =
+        inputStorage[i].uniqueId;
+      lists.append(clone);
+    }
   }
 }
 
