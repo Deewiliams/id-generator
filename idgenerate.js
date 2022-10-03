d9b1a3b1b545fa7b1589bc485f5ad2ee5ab251ee
Desire <desireirankundawilliams@gmail.com>
2022-10-03 14:25:24 +0200

    able to display information

--- idgenerate.js
+++ idgenerate.js
@@ -1,17 +1,42 @@
-
 let today = new Date();
 let YearCode = today.getFullYear();
 let idgeneration = Date.now();
-let uniqueId = YearCode + "" + idgeneration
+let uniqueId = YearCode + "" + idgeneration;
 function getUser() {
-    let fullName = document.getElementById("fullName").value;
-    let age = document.getElementById("age").value;
-    let gender = document.getElementById("gender").value;
-    let address = document.getElementById("address").value;
-    let state = document.getElementById("state").value;
-    let mobile = document.getElementById("mobile").value;
-    var displayInfo = document.createElement("li");
-    const textNode = document.createTextNode(fullName + "" + age + "" + gender + "" + address + "" +  state + "" + mobile + "" + uniqueId);
-    displayInfo.appendChild(textNode);
-    document.getElementById('generatedId').appendChild(displayInfo);
+  const inputStorage = [];
+  let fullName = document.getElementById("fullName").value;
+  let age = document.getElementById("age").value;
+  let gender = document.getElementById("gender").value;
+  let address = document.getElementById("address").value;
+  let state = document.getElementById("state").value;
+  let mobile = document.getElementById("mobile").value;
+
+  inputStorage.push({
+    fullname: fullName,
+    age: age,
+    gender: gender,
+    address: address,
+    state: state,
+    mobile: mobile,
+    uniqueId: uniqueId,
+  });
+  for (var i = 0; i < inputStorage.length; i++) {
+    const template = document.querySelector("#template");
+    const lists = document.querySelector(".lists");
+
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector(".template-city").textContent =  inputStorage[i].fullname;
+    clone.querySelector(".template-country").textContent = inputStorage[i].age;
+    lists.append(clone);
+  }
+
+  // var displayInfo = document.createElement(`
+  // <table>
+
+  // </table>
+  // `);
+  // const textNode = document.createTextNode(fullName + "" + age + "" + gender + "" + address + "" +  state + "" + mobile + "" + uniqueId);
+  // displayInfo.appendChild(textNode);
+  // document.getElementById('generatedId').appendChild(displayInfo);
 }
