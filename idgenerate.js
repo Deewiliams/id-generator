265436225c2321a86c801fe48a1620bbfaad9c1c
Desire <desireirankundawilliams@gmail.com>
2022-10-03 15:43:16 +0200

    able to generate different id numbers

--- idgenerate.js
+++ idgenerate.js
@@ -1,9 +1,11 @@
-let today = new Date();
-let YearCode = today.getFullYear();
-let idgeneration = Date.now();
-let uniqueId = YearCode + "" + idgeneration;
 function getUser() {
   const inputStorage = [];
+
+  let today = new Date();
+  let YearCode = today.getFullYear();
+  let idgeneration = Date.now();
+  let uniqueId = YearCode + "" + idgeneration;
+
   let fullName = document.getElementById("fullName").value;
   let age = document.getElementById("age").value;
   let gender = document.getElementById("gender").value;
@@ -20,18 +22,25 @@ function getUser() {
     mobile: mobile,
     uniqueId: uniqueId,
   });
-  for (var i = 0; i < inputStorage.length; i++) {
+
+  for (let i = 0; i < inputStorage.length; i++) {
     const template = document.querySelector("#template");
     const lists = document.querySelector(".lists");
 
     const clone = template.content.cloneNode(true);
 
-    clone.querySelector(".template-name").textContent =  inputStorage[i].fullname;
+    clone.querySelector(".template-name").textContent =
+      inputStorage[i].fullname;
     clone.querySelector(".template-age").textContent = inputStorage[i].age;
-    clone.querySelector(".template-gender").textContent = inputStorage[i].gender;
-    clone.querySelector(".template-address").textContent = inputStorage[i].state;
-    clone.querySelector(".template-mobile").textContent = inputStorage[i].mobile;
-    clone.querySelector(".template-uniqueId").textContent = inputStorage[i].uniqueId;
+    clone.querySelector(".template-gender").textContent =
+      inputStorage[i].gender;
+    clone.querySelector(".template-address").textContent =
+      inputStorage[i].address;
+    clone.querySelector(".template-state").textContent = inputStorage[i].state;
+    clone.querySelector(".template-mobile").textContent =
+      inputStorage[i].mobile;
+    clone.querySelector(".template-uniqueId").textContent =
+      inputStorage[i].uniqueId;
     lists.append(clone);
   }
 }
